a98960fd6ffed446a0147f57e4442a283fdcf283
Hunter Chen <hunterc23@nycstudents.net>
2026-04-27 13:33:28 -0400

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,15 +1,1 @@
-public class Student {
-    private String student_first="";
 
-    private String student_last="";
-
-    public Student(String student_first, String student_last) {
-        this.student_first = student_first;
-        this.student_last = student_last;
-    }
-
-
-    public String toString() {
-        return "INSERT INTO student (student_first, student_last) VALUES ('" + student_first + "', '" + student_last + "');";
-    }
-}
