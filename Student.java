9a60d290a878d99018145d14a534f6db484079a1
Hunter Chen <hunterc23@nycstudents.net>
2026-05-05 13:54:28 -0400

    Add studentId and count to Student class

--- Student.java
+++ Student.java
@@ -1,13 +1,23 @@
 public class Student {
    private String studentFirst="";
    private String studentLast="";
+   private int studentId;
+   private static int count = 1;
 
 
    public Student(String studentFirst, String studentLast) {
        this.studentFirst = studentFirst;
        this.studentLast = studentLast;
+       this.studentId = count;
+       count++;
    }
-  
+
+
+   public int getstudentId() {
+       return studentId;
+   }
+
+
    public String toString() {
        return "INSERT INTO student (student_first, student_last) VALUES ('" + studentFirst + "', '" + studentLast + "');";
    }
