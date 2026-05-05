c5402ed08fea9fc4e77d7d7c13e4b4d0d699e8b9
Xuhao Zhang <xuhaoz@nycstudents.net>
2026-05-05 13:18:10 -0400

    Student class - fixed naming convention

--- Student.java
+++ Student.java
@@ -1,15 +1,14 @@
 public class Student {
-    private String student_first="";
-
-    private String student_last="";
-
-    public Student(String student_first, String student_last) {
-        this.student_first = student_first;
-        this.student_last = student_last;
-    }
+   private String studentFirst="";
+   private String studentLast="";
 
 
-    public String toString() {
-        return "INSERT INTO student (student_first, student_last) VALUES ('" + student_first + "', '" + student_last + "');";
-    }
+   public Student(String studentFirst, String studentLast) {
+       this.studentFirst = studentFirst;
+       this.studentLast = studentLast;
+   }
+  
+   public String toString() {
+       return "INSERT INTO student (student_first, student_last) VALUES ('" + studentFirst + "', '" + studentLast + "');";
+   }
 }
