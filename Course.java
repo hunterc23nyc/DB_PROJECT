c36f8e43c0bd5cd96fda53cc2a33e50628c4035e
Xuhao Zhang <xuhaoz@nycstudents.net>
2026-05-05 13:13:33 -0400

    Fixed script error and naming convention

--- Course.java
+++ Course.java
@@ -4,34 +4,34 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Course {
-    String courseName;
-    String courseType;
+   String courseName;
+   String courseType;
 
-    public Course(String courseName, String courseType) {
-        this.courseName = courseName;
-        this.courseType = courseType;
-    }
-    public String getCourseName(){
-        return courseName;
-    }
-    public String getCourseType() {
-        return courseType;
-    }
-    public String toString() {
-        return "INSERT INTO course (course_name, course_type) VALUES ('" + courseName + "', '" + courseType + "');";
-    }
+   public Course(String courseName, String courseType) {
+       this.courseName = courseName;
+       this.courseType = courseType;
+   }
+   public String getCourseName(){
+       return courseName;
+   }
+   public String getCourseType() {
+       return courseType;
+   }
+   public String toString() {
+       return "INSERT INTO course (course_name, course_type) VALUES ('" + courseName + "', '" + courseType + "');";
+   }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Course> courses = new ArrayList<Course>();
-        try (Scanner scanner = new Scanner(new File("src/Courses.txt"))) {
-            while (scanner.hasNextLine()) {
-                String courseLine = scanner.nextLine();
-                String[] parts = courseLine.split("\\|");
-                courses.add(new Course(parts[0],parts[1]));
-            }
-        }
-        for (int i = courses.size() - 1; i >= 0; i--) {
-            System.out.println(courses.get(i).toString());
-        }
-    }
+   public static void main(String[] args) throws FileNotFoundException {
+       ArrayList<Course> courses = new ArrayList<Course>();
+       try (Scanner scanner = new Scanner(new File("src/Courses.txt"))) {
+           while (scanner.hasNextLine()) {
+               String courseLine = scanner.nextLine();
+               String[] parts = courseLine.split("\\|");
+               courses.add(new Course(parts[0],parts[1]));
+           }
+       }
+       for (int i = courses.size() - 1; i >= 0; i--) {
+           System.out.println(courses.get(i).toString());
+       }
+   }
 }
