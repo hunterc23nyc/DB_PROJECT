6bd48cc1cd5a9190445d212fd8ea77d9691c23ed
Xuhao Zhang <xuhaoz@nycstudents.net>
2026-05-19 12:52:39 -0400

    Implement unique ID generation for Department instances
    Refactored Department class to use a static counter for unique IDs.

--- Department.java
+++ Department.java
@@ -1,20 +1,25 @@
 public class Department {
-   public int getId() {
-       return id;
-   }
 
-   public String getName() {
-       return name;
-   }
-	
+
    private String name;
    private int id;
+   private static int counter = 1;
+
 
    public Department(String name){
        this.name = name;
-       this.id =id;
+       this.id =counter;
+       counter++;
    }
-
+  
+   public int getId() {
+       return id;
+   }
+  
+   public String getName() {
+       return name;
+   }
+  
    public String toString(){
        return "INSERT INTO department (department_name) VALUES ('" + name + "');";
    }
