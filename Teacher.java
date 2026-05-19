d2f375c3be80add90c16456e889895bb06d2bae6
Xuhao Zhang <xuhaoz@nycstudents.net>
2026-05-19 13:02:49 -0400

    Add static count field to Teacher class

--- Teacher.java
+++ Teacher.java
@@ -3,28 +3,37 @@ public class Teacher {
    private String first;
    private String last;
    private int depId;
+   private static int count = 1;
+
 
    public Teacher( int depId, String first, String last){
        this.first=first;
        this.last=last;
+       this.id=count;
        this.depId=depId;
+       count++;
    }
+
 
    public String getFirst() {
        return first;
    }
 
+
    public int getId() {
        return id;
    }
+
 
    public int getDepId() {
        return depId;
    }
 
+
    public String getLast() {
        return last;
    }
+
 
    public String toString() {
        return "INSERT INTO teacher (teacher_first, teacher_last, department_id) VALUES ('" + first + "', '" + last + "', " + depId + ");";
