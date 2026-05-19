4fe8f793dd4e7be41190cb9d0bf40cbecc72cfb9
Xuhao Zhang <xuhaoz@nycstudents.net>
2026-05-19 13:00:12 -0400

    Refactor Course class by removing main method
    Removed the main method and related course loading logic.

--- Course.java
+++ Course.java
@@ -6,7 +6,7 @@ import java.io.File;
 public class Course {
    String courseName;
    String courseType;
-
+   
    public Course(String courseName, String courseType) {
        this.courseName = courseName;
        this.courseType = courseType;
@@ -19,19 +19,5 @@ public class Course {
    }
    public String toString() {
        return "INSERT INTO course (course_name, course_type) VALUES ('" + courseName + "', '" + courseType + "');";
-   }
-
-   public static void main(String[] args) throws FileNotFoundException {
-       ArrayList<Course> courses = new ArrayList<Course>();
-       try (Scanner scanner = new Scanner(new File("src/Courses.txt"))) {
-           while (scanner.hasNextLine()) {
-               String courseLine = scanner.nextLine();
-               String[] parts = courseLine.split("\\|");
-               courses.add(new Course(parts[0],parts[1]));
-           }
-       }
-       for (int i = courses.size() - 1; i >= 0; i--) {
-           System.out.println(courses.get(i).toString());
-       }
    }
 }
